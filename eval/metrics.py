821ca89dd781f208da4a861a6d28017fe1dd89c2
Pavan Mandava <mspavan04@gmail.com>
2020-04-27 11:46:02 +0200

    Added Result Model class

--- eval/metrics.py
+++ eval/metrics.py
@@ -1,6 +1,15 @@
-
-
 def f1_score(y_true, y_pred, labels, average):
-
     return 0
 
+
+class Result:
+
+    def __init__(self, precision, recall, average, label):
+        self.precision = precision
+        self.recall = recall
+        self.average = average
+        self.label = label
+        self.f1_score = 2 * (precision * recall) / (precision + recall)
+
+    def print_result(self):
+        print('F1 Score :: ',self.f1_score)
