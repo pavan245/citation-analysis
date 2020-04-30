5254293d27554bba0a20dd298b6e1e695fba9b06
Isaac Riley <isaac.r.riley@gmail.com>
2020-04-30 20:58:31 +0200

    Test Code commit

--- testing/eval_testing.py
+++ testing/eval_testing.py
@@ -4,7 +4,7 @@ import utils.constants as const
 y_true = ['positive', 'positive', 'negative', 'negative', 'positive', 'positive', 'negative', 'negative']
 y_pred = ['positive', 'negative', 'negative', 'positive', 'positive', 'negative', 'negative', 'negative']
 
-result_list = f1_score(y_true, y_pred, ['positive', 'negative'], const.AVG_MACRO)
+result_list = f1_score(y_true, y_pred, ['positive', 'negative'], const.AVG_MICRO)
 
 for result in result_list:
     result.print_result()
