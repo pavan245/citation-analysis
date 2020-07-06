5daea1a2a80ca6cd58f6f969fcd26cd6e90a92c2
Pavan Mandava <mspavan04@gmail.com>
2020-07-06 09:10:44 +0200

    moved Isaac's commit to the current package

--- utils/reader.py
+++ utils/reader.py
@@ -1,21 +1,21 @@
-from typing import Iterable, List
-from overrides import overrides
+from typing import Iterable
+
 import jsonlines
-from utils.data import Citation
-
-from allennlp.data.fields import TextField, LabelField, MultiLabelField
-from allennlp.data import Instance, Tokenizer
+from allennlp.data import Instance
 from allennlp.data.dataset_readers import DatasetReader
+from allennlp.data.fields import TextField, LabelField
 from allennlp.data.token_indexers import SingleIdTokenIndexer, ELMoTokenCharactersIndexer
+from allennlp.data.tokenizers import SpacyTokenizer
+from overrides import overrides
 
-import utils.constants as const
+from utils.data import Citation
 
 
 @DatasetReader.register("citation_dataset_reader") # type for config files
 class CitationDataSetReader(DatasetReader):
     def __init__(self):
         super().__init__()
-        self.tokenizer = Tokenizer()
+        self.tokenizer = SpacyTokenizer()
 
     @overrides
     def _read(self, file_path: str) -> Iterable[Instance]:
@@ -25,14 +25,13 @@ class CitationDataSetReader(DatasetReader):
 
     @overrides
     def text_to_instance(self, citation_text: str,
-                         intent: List[str]) -> Instance:
+                         intent: str) -> Instance:
         citation_tokens = self.tokenizer.tokenize(citation_text)
         token_indexers = {"elmo": ELMoTokenCharactersIndexer(),
                           "tokens": SingleIdTokenIndexer()}
 
         fields = {'tokens': TextField(citation_tokens, token_indexers),
-                  'label': MultiLabelField([const.CLASS_LABELS[i] for i in intent], skip_indexing=True,
-                                           num_labels=len(const.CLASS_LABELS))}
+                  'label': LabelField(intent)}
 
         return Instance(fields)
 
