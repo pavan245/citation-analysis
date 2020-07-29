d18593f869c3048b917145c388e6ab28e5e9b2e6
Pavan Mandava <mspavan04@gmail.com>
2020-07-29 20:29:59 +0200

    printing confusion matrix

--- run.py
+++ run.py
@@ -5,4 +5,8 @@ import eval.metrics as metrics
 
 y_pred, y_true = pred.load_model_and_predict_test_data("/mount/arbeitsdaten/studenten1/team-lab-nlp/mandavsi_rileyic/saved_models/experiment_4")
 
-metrics.plot_confusion_matrix(metrics.get_confusion_matrix(y_true, y_pred), "BiLSTM Classifier + Attention with ELMo")
+confusion_matrix = metrics.get_confusion_matrix(y_true, y_pred)
+
+print(confusion_matrix)
+
+metrics.plot_confusion_matrix(confusion_matrix, "BiLSTM Classifier + Attention with ELMo")
