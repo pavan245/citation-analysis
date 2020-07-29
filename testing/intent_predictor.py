3455af4e400831dfed102271c29c6dac0f755480
Sai Pavan Mandava <mandavsi@kiwi.ims.uni-stuttgart.de>
2020-07-29 19:21:56 +0200

    Wrong Test file path fixed

--- testing/intent_predictor.py
+++ testing/intent_predictor.py
@@ -51,8 +51,8 @@ def make_predictions(model: Model, dataset_reader: DatasetReader, file_path: str
 
 def load_model_and_run_predictions(saved_model_dir: str):
     project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    train_file_path = project_root + '/data/tsv/train.tsv'
-    test_file_path = project_root + '/data/tsv/test.tsv'
+    dev_file_path = project_root + '/data/jsonl/dev.jsonl'
+    test_file_path = project_root + '/data/jsonl/test.jsonl'
 
     model_archive = load_archive(os.path.join(saved_model_dir, 'model.tar.gz'))
     citation_dataset_reader = CitationDataSetReader()
